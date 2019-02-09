b29669754ad1c61a82a8f2b9ca2e8a22fa065047
Adam Fleishaker <adam@adamfly.me>
2019-02-09 01:59:01 -0500

    Voter info fix

--- beanpolitics/src/components/VoterInfo.js
+++ beanpolitics/src/components/VoterInfo.js
@@ -11,23 +11,23 @@ class VoterInfo extends Component {
   }
 
   render() {
-    return <div className="VoterInfo">
-        <h1>Voter Info:</h1>
-        <Autocomplete
-            style={{width: '100%'}}
-            onPlaceSelected={(place) => {
-                if(place.formatted_address){
-                    this.props.getVoterInfo(place.formatted_address);
-                }
-                else{
-                    this.props.getVoterInfo(place.name);
-                }
-            }}
-            types={[]}
-            componentRestrictions={{country: "us"}}
-        />
-        <p>{this.infoOutput().toString()}</p>
-    </div>;
+      return <div className="VoterInfo">
+          <h1>Voter Info:</h1>
+          <Autocomplete
+              style={{width: '100%'}}
+              onPlaceSelected={(place) => {
+                  if(place.formatted_address){
+                      this.props.getVoterInfo(place.formatted_address);
+                  }
+                  else{
+                      this.props.getVoterInfo(place.name);
+                  }
+              }}
+              types={[]}
+              componentRestrictions={{country: "us"}}
+          />
+          <p>{this.infoOutput().toString()}</p>
+      </div>;
   }
 }
 
