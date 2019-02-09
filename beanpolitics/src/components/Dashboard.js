b601eac1a54cd447a464efea4569ec8cc2d21e09
Farquhar-Reese <reesef908@gmail.com>
2019-02-08 23:57:59 -0500

    added css styling guys :D for the home page

--- beanpolitics/src/components/Dashboard.js
+++ beanpolitics/src/components/Dashboard.js
@@ -1,10 +1,39 @@
 import React, {Component} from 'react';
+import{Button} from "antd";
+import './Global.css';
 
 class Dashboard extends Component {
     render(){
         return (
-           <div>Hi</div>
-        );
+            <html>
+           <body>
+           <div className="Title">
+               <p className="TitleMain">Bean Politics</p>
+               <p1 className="TitleSubText">sub header</p1>
+           </div>
+           <main className="MainMargins">
+               <div className="trapezoid3">
+                   <div className="trapezoid4">
+                       <p className="TrapLetters1"><i>Let's Begin</i></p>
+                   </div>
+               </div>
+               <div>
+                   <p>Voter Info</p>
+               </div>
+               <div>
+                   <p>Covfefe</p>
+               </div>
+               <div>
+                   <p>Data Archive</p>
+               </div>
+
+
+
+
+           </main>
+           </body>
+            </html>
+        )
     }
 }
 
