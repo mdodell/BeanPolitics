29dddbeb6b43a5b032962836b92afa74de99a466
Farquhar-Reese <reesef908@gmail.com>
2019-02-09 17:52:39 -0500

    FurtherCustomizedHomePage
    title is self explainatory, this includes images, color theme, css animations, and more

--- beanpolitics/src/components/Dashboard.js
+++ beanpolitics/src/components/Dashboard.js
@@ -1,38 +1,92 @@
 import React, {Component} from 'react';
 import{Button} from "antd";
 import './Global.css';
+import CongressPic from "../images/whiteHouse.jpg";
 
 class Dashboard extends Component {
     render(){
         return (
-            <html>
+
            <body>
+           <div className="BackgroundMain">
+               <img src={CongressPic} className="BackgroundImage" alt="ErrorInBackgroundImg"></img>
+           </div>
            <div className="Title">
                <p className="TitleMain">Bean Politics</p>
-               <p1 className="TitleSubText"><i>sub header</i></p1>
+               <p className="TitleSubText"><i>sub header</i></p>
            </div>
-           <main className="MainMargins">
-               <div className="trapezoid3">
-                   <div className="trapezoid4">
-                       <p className="TrapLetters1"><i>Let's Begin</i></p>
+           <main>
+               <div className="MainMargins">
+                   <div className="trapezoid3">
+                       <div className="trapezoid4">
+                           <p className="TrapLetters1"><i>Let's Begin</i></p>
+                       </div>
                    </div>
                </div>
-               <div>
-                   <p>Voter Info</p>
+               <div className="wave">
                </div>
-               <div>
-                   <p>Covfefe</p>
+               <div className="ContentContainer">
+               <div className="ContentBox">
+                   <div className="InnerImage InnerImageSpec1">
+                       <div className="InnerTitleTxt">
+                           <p>Voter Info</p>
+                       </div>
+                   </div>
+                   <div className="InnerDes">
+                       <p>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna
+                           aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                           ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit
+                           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                           occaecat cupidatat non proident, sunt in culpa qui officia
+                           deserunt mollit anim id est laborum.
+                       </p>
+                   </div>
                </div>
-               <div>
-                   <p>Data Archive</p>
+               <div className="ContentBox">
+                   <div className="InnerImage InnerImageSpec1">
+                       <div className="InnerTitleTxt">
+                           <p>Covfefe</p>
+                       </div>
+                   </div>
+                   <div className="InnerDes">
+                       <p>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna
+                           aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                           ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit
+                           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                           occaecat cupidatat non proident, sunt in culpa qui officia
+                           deserunt mollit anim id est laborum.
+                       </p>
+                   </div>
                </div>
-
-
-
-
+               <div className="ContentBox">
+                   <div className="InnerImage InnerImageSpec3">
+                       <div className="InnerTitleTxt">
+                           <p>Data Archive</p>
+                       </div>
+                   </div>
+                   <div className="InnerDes">
+                       <p>
+                           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                           sed do eiusmod tempor incididunt ut labore et dolore magna
+                           aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+                           ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                           Duis aute irure dolor in reprehenderit in voluptate velit
+                           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
+                           occaecat cupidatat non proident, sunt in culpa qui officia
+                           deserunt mollit anim id est laborum.
+                       </p>
+                   </div>
+               </div>
+               </div>
            </main>
            </body>
-            </html>
+
         )
     }
 }
