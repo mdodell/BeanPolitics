0ea70112cd57cd1822722a750615f697fb672409
Adam Fleishaker <adam@adamfly.me>
2019-02-09 19:43:32 -0500

    Dashboard fixes

--- beanpolitics/src/components/Dashboard.js
+++ beanpolitics/src/components/Dashboard.js
@@ -1,25 +1,25 @@
 import React, {Component} from 'react';
 import{Button} from "antd";
 import './Global.css';
-import CongressPic from "../images/whiteHouse.jpg";
+import CongressPic from "../images/Congress.jpg";
 
 class Dashboard extends Component {
     render(){
         return (
 
-           <body>
+           <div>
            <div className="BackgroundMain">
                <img src={CongressPic} className="BackgroundImage" alt="ErrorInBackgroundImg"></img>
            </div>
            <div className="Title">
-               <p className="TitleMain">Bean Politics</p>
-               <p className="TitleSubText"><i>sub header</i></p>
+               <p className="TitleMain"><i className="fas fa-flag-usa"></i>&nbsp;BeanPolitics</p>
+               <p className="TitleSubText"><i>A fun, educational, and information-rich hub app for all things politics!</i></p>
            </div>
            <main>
                <div className="MainMargins">
                    <div className="trapezoid3">
                        <div className="trapezoid4">
-                           <p className="TrapLetters1"><i>Let's Begin</i></p>
+                           <p className="TrapLetters1"><i>Made at HackBeanpot!</i></p>
                        </div>
                    </div>
                </div>
@@ -27,65 +27,44 @@ class Dashboard extends Component {
                </div>
                <div className="ContentContainer">
                <div className="ContentBox">
-                   <div className="InnerImage InnerImageSpec1">
+                   <div className="InnerImage InnerImageSpec1" onClick={function(){window.location.href='/voterInfo'}}>
                        <div className="InnerTitleTxt">
-                           <p>Voter Info</p>
+                           <h1 className="emphasis">Civic Info Hub</h1>
                        </div>
                    </div>
                    <div className="InnerDes">
-                       <p>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                           sed do eiusmod tempor incididunt ut labore et dolore magna
-                           aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                           ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                           Duis aute irure dolor in reprehenderit in voluptate velit
-                           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                           occaecat cupidatat non proident, sunt in culpa qui officia
-                           deserunt mollit anim id est laborum.
-                       </p>
+                       <h2 className="emphasis">
+                           Want to find out who represents you? Use the Civic Info Hub to become informed on your local representatives and how to contact them!
+                       </h2>
                    </div>
                </div>
                <div className="ContentBox">
-                   <div className="InnerImage InnerImageSpec2">
+                   <div className="InnerImage InnerImageSpec2" onClick={function() {window.location.href='/covfefe'}}>
                        <div className="InnerTitleTxt">
-                           <p>Covfefe</p>
+                           <h1 className="emphasis">Covfefe.io</h1>
                        </div>
                    </div>
                    <div className="InnerDes">
-                       <p>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                           sed do eiusmod tempor incididunt ut labore et dolore magna
-                           aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                           ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                           Duis aute irure dolor in reprehenderit in voluptate velit
-                           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                           occaecat cupidatat non proident, sunt in culpa qui officia
-                           deserunt mollit anim id est laborum.
-                       </p>
+                       <h2 className="emphasis">
+                           A fun, informative game where users determine whether or not articles that appear are "Fake News". Try your luck at Covfefe.io!
+                       </h2>
                    </div>
                </div>
                <div className="ContentBox">
                    <div className="InnerImage InnerImageSpec3">
                        <div className="InnerTitleTxt">
-                           <p>Data Archive</p>
+                           <h1 className="emphasis">Data Archive</h1>
                        </div>
                    </div>
                    <div className="InnerDes">
-                       <p>
-                           Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-                           sed do eiusmod tempor incididunt ut labore et dolore magna
-                           aliqua. Ut enim ad minim veniam, quis nostrud exercitation
-                           ullamco laboris nisi ut aliquip ex ea commodo consequat.
-                           Duis aute irure dolor in reprehenderit in voluptate velit
-                           esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-                           occaecat cupidatat non proident, sunt in culpa qui officia
-                           deserunt mollit anim id est laborum.
-                       </p>
+                       <h2 className="emphasis">
+                           Curious about past election results? Access the Data Archive to see visualizations of your state's presidential and Senate elections!
+                       </h2>
                    </div>
                </div>
                </div>
            </main>
-           </body>
+           </div>
 
         )
     }
