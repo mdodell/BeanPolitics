437d6e9ae61ad3a9e87592323da9b020e95a7c3e
mdodell <dodellmitchell@gmail.com>
2019-02-11 00:20:52 -0500

    Updates

---
+++ precache-manifest.144eadb62351de0af333636d990d7bd7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a89fdd07c491a08badf30e24bd381e4",
+    "url": "/BeanPolticis/static/media/placeholder.1a89fdd0.png"
+  },
+  {
+    "revision": "d9a3507880e61b4906c6",
+    "url": "/BeanPolticis/static/css/main.f6abe2d4.chunk.css"
+  },
+  {
+    "revision": "3dc4eefa33529b0c6bf368dd1041ad89",
+    "url": "/BeanPolticis/static/media/matrixCode.3dc4eefa.jpg"
+  },
+  {
+    "revision": "2f92113ae493a1ff4fc9",
+    "url": "/BeanPolticis/static/js/1.2f92113a.chunk.js"
+  },
+  {
+    "revision": "0130cb6b60e478b7c459",
+    "url": "/BeanPolticis/static/js/runtime~main.0130cb6b.js"
+  },
+  {
+    "revision": "d9a3507880e61b4906c6",
+    "url": "/BeanPolticis/static/js/main.d9a35078.chunk.js"
+  },
+  {
+    "revision": "4c4b2e5b008a4040b7b4266bebddb3ac",
+    "url": "/BeanPolticis/static/media/whiteHouse.4c4b2e5b.jpg"
+  },
+  {
+    "revision": "d3d166392e0714dd83c3446679a85314",
+    "url": "/BeanPolticis/static/media/fakeNews.d3d16639.wav"
+  },
+  {
+    "revision": "6d533586a02c7f8268074065882f7c46",
+    "url": "/BeanPolticis/static/media/Congress.6d533586.jpg"
+  },
+  {
+    "revision": "2f92113ae493a1ff4fc9",
+    "url": "/BeanPolticis/static/css/1.861fc192.chunk.css"
+  },
+  {
+    "revision": "c6557c662ce3091a0631e749959b3abf",
+    "url": "/BeanPolticis/index.html"
+  }
+];
